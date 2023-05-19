4d0b5afbcff1df3f723c420fc21ff9962890b116
Singh Ayush <79689279+ayushksingh28@users.noreply.github.com>
2023-05-19 22:57:41 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+
+    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
